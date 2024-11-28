9141cf7e1c7ccae249f0785f2706bc2bcd7fbcca
zoulja <zoulja@users.noreply.github.com>
2024-11-28 11:41:05 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+// service-worker.js
+self.addEventListener('install', (event) => {
+  console.log('Service Worker: Установлен');
+  event.waitUntil(
+    caches.open('static-v1').then((cache) => {
+      return cache.addAll([
+        '/', // Главная страница
+        '/index.html',
+        '/style.css',
+        '/script.js',
+        '/manifest.json',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  console.log('Service Worker: Обрабатываю запрос', event.request.url);
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker: Активирован');
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== 'static-v1') {
+            console.log('Service Worker: Удаляю старый кэш', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
